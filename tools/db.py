9b78de92f3843079826f9f424166272bb233156f
Topleft <87454455+topleft1562@users.noreply.github.com>
2025-05-09 17:21:48 -0500

    ok

--- tools/db.py
+++ tools/db.py
@@ -5,19 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI")
-MONGODB_URI_SOLFORGE = os.getenv("MONGODB_URI_SOLFORGE")
+
 
 if not MONGODB_URI:
     raise ValueError("Missing MONGODB_URI in .env")
-if not MONGODB_URI_SOLFORGE:
-    raise ValueError("Missing MONGODB_URI_SOLFORGE in .env")
+
 
 client = MongoClient(MONGODB_URI)
-clientSOLFORGE = MongoClient(MONGODB_URI_SOLFORGE)
+
 
 # ✅ Explicitly select the correct DB
 db = client["mainnet-beta"]
-dbSOLFORGE = client["main"]
+
 
 # Collections - FATCAT
 group_subs = db['groupSubscriptions']
@@ -25,10 +24,5 @@ projects = db['projects']
 raids = db['raids']
 users = db['users']
 
-# Collections - SOLFORGE
-coins = dbSOLFORGE['coins']
-coinstatuses = dbSOLFORGE['coinstatuses']
-scratchhistories = dbSOLFORGE['scratchhistories']
-spinhistories = dbSOLFORGE['spinhistories']
-solforge_users = dbSOLFORGE['users']
+
 
