10e63604f865e1fccb04b6d11171b13733756981
Topleft <87454455+topleft1562@users.noreply.github.com>
2025-06-11 07:08:56 -0500

    kk

--- utils.py
+++ utils.py
@@ -1,20 +1,26 @@
-def generate_google_map_html(pin_coords, google_key):
-    pin_markers = [
-        f"new google.maps.Marker({{ position: {{ lat: {lat}, lng: {lng} }}, map: map, title: '{city}' }});"
-        for city, (lat, lng) in pin_coords.items() if lat is not None and lng is not None
-    ]
+def generate_google_map_html(pin_coords: dict, google_key: str) -> str:
+    markers_script = ""
+    for city, coords in pin_coords.items():
+        if coords is None:
+            continue
+        lat, lng = coords
+        markers_script += f"""
+        new google.maps.Marker({{
+            position: {{ lat: {lat}, lng: {lng} }},
+            map: map,
+            title: "{city}"
+        }});\n"""
 
     return f"""
-    <div id='map' style='height: 600px; width: 100%; margin-bottom: 20px;'></div>
-    <script src='https://maps.googleapis.com/maps/api/js?key={google_key}'></script>
+    <div id="map" style="height: 500px; width: 100%; margin-bottom: 20px;"></div>
     <script>
     function initMap() {{
         const map = new google.maps.Map(document.getElementById('map'), {{
             zoom: 5,
-            center: {{ lat: 50.0, lng: -100.0 }}
+            center: {{ lat: 50.0, lng: -100.0 }},
         }});
-        {''.join(pin_markers)}
+        {markers_script}
     }}
-    window.onload = initMap;
     </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={google_key}&callback=initMap"></script>
     """
